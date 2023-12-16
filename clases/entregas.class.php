0fb364a240aa0d011298bf70e13d5a10fe66cc65
Andres <andresf150@gmail.com>
2023-12-16 12:05:41 -0600

    🚧 [paginacion] obtener todas las entregas incuyendo nombres

--- clases/entregas.class.php
+++ clases/entregas.class.php
@@ -6,10 +6,33 @@ require_once "respuestas.class.php";
 class entregas extends conexion {
 
     private $table = "entregas";
+    private $tableTrabajador = "trabajadores";
     private $idTrabajador = "";
     private $precioEntrega = "";
     private $cantidadEntregas = "";
     private $fecha = "";
+
+    /**
+     * Esta función devuelve listado de todas las entregas, espera el numero de pagina ya que solo trae 100 registros
+     * @var pagina int
+     * @access public
+     * @return array
+     */
+    public function listaEntregas($pagina = 1){
+        $inicio  = 0 ;
+        $cantidad = 100;
+        if($pagina > 1){
+            $inicio = ($cantidad * ($pagina - 1)) +1 ;
+            $cantidad = $cantidad * $pagina;
+        }
+        $query = "SELECT entregas.idEntrega,trabajadores.nombreCompleto,entregas.precioEntrega,entregas.cantidadEntregas,entregas.fecha 
+        FROM entregas 
+        INNER JOIN trabajadores ON entregas.idTrabajador = trabajadores.idTrabajador" . " limit $inicio,$cantidad;";
+
+
+        $datos = parent::obtenerDatos($query);
+        return ($datos);
+    }
 
 
      
