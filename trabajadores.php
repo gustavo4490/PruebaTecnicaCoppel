cb1ac7cb2b247361fb8612f5bd84bdcf9c4903bb
Andres <andresf150@gmail.com>
2023-12-16 16:44:04 -0600

    🔨 [trabajadores] verbos

---
+++ trabajadores.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'clases/respuestas.class.php';
+require_once 'clases/trabajador.class.php';
+
+$_respuestas = new respuestas;
+$_trabajador = new trabajador;
+
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    if (isset($_GET["page"])) {
+        $pagina = $_GET["page"];
+        $listaTrabajadores = $_trabajador->listaTodosLosTrabajadores($pagina);
+        header("Content-Type: application/json");
+        echo json_encode($listaTrabajadores);
+        http_response_code(200);
+    } else if (isset($_GET['id'])) {
+        $IdTrabajador = $_GET['id'];
+        $datosTrabajador = $_trabajador->obtenerDatosTrabajadorPorID($IdTrabajador);
+        header("Content-Type: application/json");
+        echo json_encode($datosTrabajador);
+        http_response_code(200);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos los datos al manejador
+    $datosArray = $_trabajador->post($postBody);
+    //delvovemos una respuesta 
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+} else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos datos al manejador
+    $datosArray = $_trabajador->put($postBody);
+    //delvovemos una respuesta 
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+} else {
+    header('Content-Type: application/json');
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
