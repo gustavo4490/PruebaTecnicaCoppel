5d9e41c066ec4d7d25aab15a65903c3ada73e5b1
Andres <andresf150@gmail.com>
2023-12-16 14:10:25 -0600

    🏗️ verbo port, guardar trabajadores con procedimiento almacenado

--- clases/trabajador.class.php
+++ clases/trabajador.class.php
@@ -5,10 +5,16 @@ require_once "respuestas.class.php";
 
 class trabajador extends conexion {
 
-    
 
-   
-    
+
+    private $nombreCompleto = "";
+    private $idRol = "";
+    private $numeroEmpleado = "";
+    private $bonoPorHora = "";
+    private $sueldoPorHora = "";
+    private $valesDespensa = "";
+
+      
 
 
     /**
@@ -46,6 +52,89 @@ class trabajador extends conexion {
 
         return parent::obtenerDatos($query);
     }
+
+    /**
+     * Esta función guarda los trabajadores utilizando verbo post.
+     * @var nombreCompleto
+     * @var idRol
+     * @var numeroEmpleado
+     * @var bonoPorHora
+     * @var sueldoPorHora
+     * @var valesDespensa
+     * @access public
+     * @return array
+     */
+
+     public function post($json)
+     {
+         $_respuestas = new respuestas;
+         $datos = json_decode($json, true);
+ 
+         if ($datos === null) {
+             return $_respuestas->error_400("JSON inválido");
+         }
+ 
+         $requiredFields = array( 'nombreCompleto', 'idRol','numeroEmpleado','bonoPorHora','sueldoPorHora','valesDespensa');
+         foreach ($requiredFields as $field) {
+             if (!isset($datos[$field])) {
+                 return $_respuestas->error_400("El campo '$field' es obligatorio");
+             }
+         }
+ 
+         $this->nombreCompleto = $datos['nombreCompleto'];
+         $this->idRol = $datos['idRol'];
+         $this->numeroEmpleado = $datos['numeroEmpleado'];
+         $this->bonoPorHora = $datos['bonoPorHora'];
+         $this->sueldoPorHora = $datos['sueldoPorHora'];
+         $this->valesDespensa = $datos['valesDespensa'];
+ 
+ 
+         if (!is_string($this->nombreCompleto) || !is_string($this->bonoPorHora) || !is_string($this->sueldoPorHora)|| !is_string($this->valesDespensa)|| !is_string($this->numeroEmpleado)) {
+             return $_respuestas->error_400("Los campos nombreCompleto, sueldoPorHora, numeroEmpleado y valesDespensa deben ser de tipo string");
+         }
+ 
+         if (!is_int($this->idRol)) {
+             return $_respuestas->error_400("El campo idRol  debe ser de tipo entero");
+         }
+ 
+         $resp = $this->insertarTrabajador();
+         if ($resp) {
+             $respuesta = $_respuestas->ok_200_procedimientos_almacenados('Datos almacenados correctamente');
+             return $respuesta;
+         } else {
+             return $_respuestas->error_500();
+         }
+     }
+ 
+     /**
+      * Esta función inserta a la base de datos las entregas .
+      * @access private
+      * @return boolen
+      */
+ 
+     private function insertarTrabajador()
+     {
+ 
+         $params = array(
+             //s= string , i = int
+                                                   
+             array('type' => 's', 'value' => $this->nombreCompleto),
+             array('type' => 'i', 'value' => $this->idRol),
+             array('type' => 's', 'value' => $this->numeroEmpleado),
+             array('type' => 's', 'value' => $this->bonoPorHora),
+             array('type' => 's', 'value' => $this->sueldoPorHora),
+             array('type' => 's', 'value' => $this->valesDespensa)
+ 
+         );
+         $result = $this->executeStoredProcedure('InsertarTrabajador', $params);
+         if ($result) {
+             // echo 'El procedimiento se ejecutó correctamente.';
+             return true;
+         } else {
+             // echo 'Ocurrió un error al ejecutar el procedimiento.';
+             return false;
+         }
+     }
 
 
    
