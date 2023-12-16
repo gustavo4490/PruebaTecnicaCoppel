655d745212f25a7bfb17b477f27d5a272cef251c
Andres <andresf150@gmail.com>
2023-12-16 11:54:43 -0600

    🚧[entrega] iniciando entregas.class donde  gestionaremos verbos

---
+++ clases/entregas.class.php
@@ -0,0 +1,22 @@
+<?php
+require_once "conexion/conexion.php";
+require_once "respuestas.class.php";
+
+
+class entregas extends conexion {
+
+    private $table = "entregas";
+    private $idTrabajador = "";
+    private $precioEntrega = "";
+    private $cantidadEntregas = "";
+    private $fecha = "";
+
+
+     
+}
+
+
+
+
+
+?>
