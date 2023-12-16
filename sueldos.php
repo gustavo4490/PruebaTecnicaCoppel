22ae8907661c2467258f64c7db19cab096a13063
Andres <andresf150@gmail.com>
2023-12-16 17:23:13 -0600

    manjear verbos get y post sueldos

---
+++ sueldos.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'clases/respuestas.class.php';
+require_once 'clases/sueldos.class.php';
+
+$_respuestas = new respuestas;
+$_sueldo = new sueldo;
+
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    if (isset($_GET["page"])) {
+        $pagina = $_GET["page"];
+        $listaSueldos = $_sueldo->listaTodosLosSueldos($pagina);
+        header("Content-Type: application/json");
+        echo json_encode($listaSueldos);
+        http_response_code(200);
+    } else if (isset($_GET['id'])) {
+        $IdTrabajador = $_GET['id'];
+        $datosTrabajador = $_sueldo->obtenerSueldoPorID($IdTrabajador);
+        header("Content-Type: application/json");
+        echo json_encode($datosTrabajador);
+        http_response_code(200);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos los datos al manejador
+    $datosArray = $_sueldo->calcularSueldo($postBody);
+    //delvovemos una respuesta 
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+}  else {
+    header('Content-Type: application/json');
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
